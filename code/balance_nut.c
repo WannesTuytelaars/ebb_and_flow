1a82edc099b8c6b3f107424380dd1fb936046b92
WannesTuytelaars <153721502+WannesTuytelaars@users.noreply.github.com>
2023-12-13 11:27:45 +0100

    Add files via upload

---
+++ code/balance_nut.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define NUTRIENTS 12
+void *balance_nut(double* concentrations, double* required_concentrations, double* masses_add){//concentrations are always in mg/L and volumes in L
+    double volume_now = *(concentrations+NUTRIENTS);
+    double volume_end = *(required_concentrations+NUTRIENTS);
+    double masses[NUTRIENTS];
+    for(int i = 0; i<NUTRIENTS; i++){
+        masses_add[i] = *(required_concentrations + i) * volume_end - *(concentrations + i) * volume_now;
+    masses_add[NUTRIENTS] = volume_end - volume_now;
+    }    
+    return  0;
+}
